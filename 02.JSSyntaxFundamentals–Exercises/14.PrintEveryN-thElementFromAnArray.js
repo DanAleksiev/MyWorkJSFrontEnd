389eebf187ddc1b62c92578f52665ea0516dcba8
DanAleksiev <toksikar46@gmail.com>
2023-06-26 14:25:14 +0100

    ...

--- 02.JSSyntaxFundamentals–Exercises/14.PrintEveryN-thElementFromAnArray.js
+++ 02.JSSyntaxFundamentals–Exercises/14.PrintEveryN-thElementFromAnArray.js
@@ -1,6 +1,13 @@
 function solve(numbers, rotations){
+    let output =[];
+for (let i = 0; i < numbers.length; i++) {
+    if(i % rotations === 0){
+        output.push(numbers[i])
+    }
+}
 
-
+return output;
 }
 
 solve(['dsa', 'asd', 'test', 'tset'], 2);
+solve(['1','2','3','4','5'],6);
