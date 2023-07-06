624c00b7f2f8345d784a9700fc27228c2cb7e59c
DanAleksiev <toksikar46@gmail.com>
2023-07-06 17:25:32 +0100

    ...

---
+++ 03.ObjectsAndClasses-Lab/03.CityTaxes.js
@@ -0,0 +1,31 @@
+function cityTaxes(name, population, treasury) {
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+
+        collectTaxes: function(){
+            this.treasury += population * this.taxRate;
+        },
+        applyGrowth: function(percentage) {
+            this.population += this.population * (percentage/100);
+        },
+        applyRecession: function(percentage) {
+            this.treasury -= this.treasury * (percentage/100);
+        }
+    };
+
+    return city;
+}
+
+const city =
+  cityTaxes('Tortuga',
+  7000,
+  15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
+city.applyRecession(10)
+console.log(city.treasury);
