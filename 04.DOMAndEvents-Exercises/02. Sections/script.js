94acf2140accb80487dcb88272fd005bdb13645b
DanAleksiev <toksikar46@gmail.com>
2023-07-15 17:49:50 +0100

    ...

--- 04.DOMAndEvents-Exercises/02. Sections/script.js
+++ 04.DOMAndEvents-Exercises/02. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+   function click(p) {
+         const currP = p.currentTarget.querySelector("p")
+         currP.style.display = "block";
+      
+   }
+   
+   words.forEach(element => {
+      const div = document.createElement("div");  
+      const paragraph = document.createElement("p")
+
+      paragraph.textContent = element;
+      paragraph.style.display = "none"
+      
+      div.appendChild(paragraph);
+      div.addEventListener("click", click)
+
+      const currentElement = document.getElementById("content").appendChild(div)
+
+   });
 }
