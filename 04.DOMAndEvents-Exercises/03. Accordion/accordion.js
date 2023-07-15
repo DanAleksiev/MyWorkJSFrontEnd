848a9a3d9950b15eb9dceae35560bb8834e3ca46
DanAleksiev <toksikar46@gmail.com>
2023-07-15 18:41:18 +0100

    ...

--- 04.DOMAndEvents-Exercises/03. Accordion/accordion.js
+++ 04.DOMAndEvents-Exercises/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+    const text = document.querySelector("#extra")
+    const target = document.querySelector("span.button")
+
+    text.style.display = text.style.display === 
+    "block" ? "none" : "block"; 
+
+    target.textContent = target.textContent === 
+    "More" ? "Less" : "More";
 }
--- 04.DOMAndEvents-Exercises/04. Locked-Profile/app.js
+++ 04.DOMAndEvents-Exercises/04. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    function clicked(e) {
+        const lockedOrNot = e.currentTarget.parentElement.querySelector("input[type=radio]");
+        
+        if(lockedOrNot.checked){
+            return;
+        }
+
+        const isHidden = e.currentTarget.textContent === "Show more";
+        const hiddenInfo = e.currentTarget.parentElement.querySelector("div");
+    
+        hiddenInfo.style.display = isHidden ? "block" : "none";
+        e.currentTarget.textContent = isHidden ? "Hide it" : "Show more";
+
+    }
+
+
+    const buttons = Array.from(document.querySelectorAll("button"))
+    buttons.forEach((b) => {
+        b.addEventListener("click", clicked)
+    })
 }
