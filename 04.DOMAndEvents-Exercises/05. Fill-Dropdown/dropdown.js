792072dc426e50b6cf6f89f12d80d6c00e5077b6
DanAleksiev <toksikar46@gmail.com>
2023-07-16 18:00:37 +0100

    ...

--- 04.DOMAndEvents-Exercises/05. Fill-Dropdown/dropdown.js
+++ 04.DOMAndEvents-Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,6 +1,7 @@
 function addItem() {
     const inputsText = document.getElementById("newItemText");
     const inputsValue = document.getElementById("newItemValue");
+    //use the element if you want to overwrite it afterwords instead of just it's value. It could be potentially more useful in the future to take the value only when you need it and leave the element as a var
     
     const targetWindow = document.getElementById("menu");
     const option = document.createElement("option");
@@ -9,12 +10,10 @@ function addItem() {
     option.value = inputsValue.value;
 
     targetWindow.appendChild(option)
-
-    // document.getElementById("newItemText").value = "";
-    // document.getElementById("newItemValue").value = "";
-
+    
     inputsText.value = "";
     inputsValue.value = "";
-
-    //use the element if you want to overwrite it afterwords instead of just it's value. It could be potentially more useful in the future to take the value only when you need it and leave the element as a var
+    
+    // document.getElementById("newItemText").value = "";
+    // document.getElementById("newItemValue").value = "";
 }
