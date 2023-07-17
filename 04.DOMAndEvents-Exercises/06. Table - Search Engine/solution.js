9332c793bbe6622c81823c591b59e2f7224317c3
DanAleksiev <toksikar46@gmail.com>
2023-07-17 09:06:58 +0100

    ...

--- 04.DOMAndEvents-Exercises/06. Table - Search Engine/solution.js
+++ 04.DOMAndEvents-Exercises/06. Table - Search Engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const data = Array.from(document.querySelectorAll("tbody tr"))
+      const target = document.querySelector("#searchField")
 
+      const activeRows = Array.from(document.querySelectorAll("tbody tr.select"));
+
+      activeRows.forEach((row) => {
+        row.classList.remove("select");
+      });
+      // data
+      // .filter((d) => d.textContent.includes(target.value))
+      // .forEach((d) => {
+      //    d.classList.add("select");
+      // });
+
+      data.forEach((d)=>{
+         if (d.textContent.includes(target.value)) {
+            d.classList.add("select")
+         }
+      })
+      target.value = ""
    }
 }
