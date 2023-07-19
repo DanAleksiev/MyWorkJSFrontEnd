872c52c977dae384687140cf8be054f978c4e3cc
DanAleksiev <toksikar46@gmail.com>
2023-07-19 09:15:48 +0100

    ...

--- 05. JS-Front-End-HTTP-and-REST-Lab-Resources/04. GitHub-Repos/scripts/app.js
+++ 05. JS-Front-End-HTTP-and-REST-Lab-Resources/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,13 @@
 function loadRepos() {
-   console.log("TODO...");
+   fetch ("https://api.github.com/users/testnakov/repos")
+   .then((res)=>res.json())
+   .then((body)=> {
+      body.forEach(repo => {
+         const name = document.createElement("h2");
+         name.textContent = repo.name;
+
+         document.querySelector("body").appendChild(name);
+      });
+   });
+   
 }
