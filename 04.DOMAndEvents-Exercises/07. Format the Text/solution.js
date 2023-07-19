4bd159aac7a80eab4b9e6331208b61ca1855b235
DanAleksiev <toksikar46@gmail.com>
2023-07-19 09:05:00 +0100

    ...

--- 04.DOMAndEvents-Exercises/07. Format the Text/solution.js
+++ 04.DOMAndEvents-Exercises/07. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  const text = document.querySelector("#input").value.split(".");
+  const output = document.getElementById("output");
+  
+  text.pop();
+  
+  while(text.length > 0){
+    let p = text
+    .splice(0, 3)
+    .map((p)=> p.trim());
+
+  const newP = document.createElement('p');
+  newP.textContent = p.join('.') + '.';
+
+  output.appendChild(newP);
+  }
 }
