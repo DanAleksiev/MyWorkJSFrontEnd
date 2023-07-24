144b80f78eaa105d7ed36414b676e5231b855981
DanAleksiev <toksikar46@gmail.com>
2023-07-24 20:06:08 +0100

    ...

--- 05. JS-Front-End-HTTP-and-REST-Exercises-Resources/05.Messenger/app.js
+++ 05. JS-Front-End-HTTP-and-REST-Exercises-Resources/05.Messenger/app.js
@@ -1,5 +1,29 @@
 function attachEvents() {
-    // TODO:
+  const textArea = document.querySelector("#messages");
+  const currentMessages = [];
+
+  const button = document.getElementById("submit");
+  button.addEventListener("click", (e) => {
+    fetch(`http://localhost:3030/jsonstore/messenger`)
+      .then((res) => res.json())
+      .then((body) => {
+        const nameField = document.querySelector('input[name="author"]').value;
+        const messageField = document.querySelector(
+          'input[name="content"]'
+        ).value;
+        for (const m of Object.keys(body)) {
+            console.log(m);
+          currentMessages.push(`${body[m].author}: ${body[m].content}`);
+        }
+
+        currentMessages.push(`${nameField}: ${messageField}`);
+      });
+  });
+
+  const display = document.getElementById("refresh");
+  display.addEventListener("click", (e) => {
+    textArea.textContent = currentMessages.join(`\n`)
+  })
 }
 
 attachEvents();
